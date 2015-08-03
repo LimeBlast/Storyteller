863cccb218442e76f483eac44347d04b6d046885
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-08-03 12:00:06 +0100

    now we're cooking with gas (and are including who did what)

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Story < ActiveRecord::Base
   include PublicActivity::Model
-  tracked
+
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
 
   belongs_to :user
 
